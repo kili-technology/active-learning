f8d4bb3e518fab51a8a33834fe170b63a040793a
mDuval1 <maxime.duval@eleves.enpc.fr>
2020-09-07 13:59:21 +0200

    Add and fix bayesian strategies

--- al/algorithms/deep_bayesian.py
+++ al/algorithms/deep_bayesian.py
@@ -5,6 +5,7 @@ import numpy as np
 from .baseline import Strategy
 from ..helpers.time import timeit
 
+
 class BayesianActiveLearning(Strategy):
 
     def __init__(self, nb_forward=10, **kwargs):
@@ -25,10 +26,28 @@ class BayesianActiveLearning(Strategy):
         return sorted_idx[-top:]
 
 
+class BayesianKLDivergence(BayesianActiveLearning):
+    @timeit
+    def score_dataset(self, dataset, learner, log_time={}):
+        stacked_probabilities = self.evaluate_dataset(
+            dataset, learner, log_time=log_time)
+        C, N, _ = stacked_probabilities.shape
+        probabilities = np.mean(stacked_probabilities, axis=0)
+        divergences = np.zeros((N, C))
+        for i in range(N):
+            for c in range(C):
+                probabilities_ic = stacked_probabilities[c, i]
+                probabilities_i = probabilities[i]
+                divergences[i, c] = np.sum(
+                    probabilities_ic * np.log(probabilities_ic/probabilities_i))
+        return np.mean(divergences, axis=1)
+
+
 class BayesianEntropyStrategy(BayesianActiveLearning):
     @timeit
     def score_dataset(self, dataset, learner, log_time={}):
-        stacked_probabilities = self.evaluate_dataset(dataset, learner, log_time=log_time)
+        stacked_probabilities = self.evaluate_dataset(
+            dataset, learner, log_time=log_time)
         probabilities = np.mean(stacked_probabilities, axis=0)
         assert len(probabilities) == len(dataset)
         entropies = -np.sum(probabilities * np.log(probabilities), axis=1)
@@ -40,8 +59,10 @@ class BayesianBALDStrategy(BayesianActiveLearning):
     def score_dataset(self, dataset, learner, log_time={}):
         inference_result = learner.inference(dataset)
         model_probabilities = inference_result['class_probabilities']
-        model_entropies = -np.sum(model_probabilities * np.log(model_probabilities), axis=1)
-        stacked_probabilities = self.evaluate_dataset(dataset, learner, log_time=log_time)
-        average_probabilities = np.mean(stacked_probabilities, axis=0)
-        average_entropies = -np.sum(average_probabilities * np.log(average_probabilities), axis=1)
+        model_entropies = - \
+            np.sum(model_probabilities * np.log(model_probabilities), axis=1)
+        stacked_probabilities = self.evaluate_dataset(
+            dataset, learner, log_time=log_time)
+        average_entropies = np.mean(
+            np.sum(stacked_probabilities * np.log(stacked_probabilities), axis=2), axis=0)
         return model_entropies - average_entropies
